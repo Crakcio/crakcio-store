9f10c1e3f79a8e22cf375ac750ca62db2fd18ab6
Crakcio <150481193+Crakcio@users.noreply.github.com>
2025-07-24 17:51:57 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 import { cargarProductos } from './products.js';
 import { actualizarCarrito, enviarPedidoWhatsApp, validarFormulario } from './ui.js';
-import { productos } from './products.js';
+import { obtenerProductos } from './products.js';
 import { mostrarProductos } from './ui.js';
 
-document.addEventListener("DOMContentLoaded", () => {
-  // Mostrar productos al cargar
+document.addEventListener("DOMContentLoaded", async () => {
+  const productos = await obtenerProductos();
   mostrarProductos(productos, "contenedor-productos");
 
-  // Evento de login básico (sin base de datos aún)
   document.getElementById("login-btn").addEventListener("click", () => {
     const email = document.getElementById("login-email").value;
     const password = document.getElementById("login-password").value;
@@ -20,6 +19,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
 
 const SUPABASE_URL = 'https://twznikjjvtoedfaxbuvf.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3em5pa2pqdnRvZWRmYXhidXZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyOTI1NDAsImV4cCI6MjA2ODg2ODU0MH0.aOQ10hq-syYqrenvFxveBQj6wKqdDtsDKfykSm42MFE'; // ⚠️ Usa tu clave pública (anon)

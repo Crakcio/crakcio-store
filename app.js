1ffcbb041d230249875afd13d457cdb7b96f2cb9
Crakcio <150481193+Crakcio@users.noreply.github.com>
2025-07-24 20:40:56 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+import { supabase } from "./supabaseClient.js";
 import { cargarProductos } from './products.js';
 import { actualizarCarrito, enviarPedidoWhatsApp, validarFormulario } from './ui.js';
 import { obtenerProductos } from './products.js';

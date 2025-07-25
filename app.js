af3758cedcd25cbdd81f0dbcce7d8ff87ed7e4fe
Crakcio <150481193+Crakcio@users.noreply.github.com>
2025-07-24 21:02:15 -0500

    Update app.js

--- app.js
+++ app.js
@@ -32,12 +32,6 @@ document.addEventListener("DOMContentLoaded", async () => {
 });
 
 
-
-const SUPABASE_URL = 'https://twznikjjvtoedfaxbuvf.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3em5pa2pqdnRvZWRmYXhidXZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyOTI1NDAsImV4cCI6MjA2ODg2ODU0MH0.aOQ10hq-syYqrenvFxveBQj6wKqdDtsDKfykSm42MFE'; // ⚠️ Usa tu clave pública (anon)
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-
 // Eventos
 document.addEventListener("DOMContentLoaded", () => {
   cargarProductos();

0b91f16ab8613883c24e3c7496a16d8334a90898
Crakcio <150481193+Crakcio@users.noreply.github.com>
2025-07-24 19:20:18 -0500

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,8 @@
-// Reemplaza con tus claves reales
+
+// supabaseClient.js
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
 const SUPABASE_URL = "https://twznikjjvtoedfaxbuvf.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3em5pa2pqdnRvZWRmYXhidXZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyOTI1NDAsImV4cCI6MjA2ODg2ODU0MH0.aOQ10hq-syYqrenvFxveBQj6wKqdDtsDKfykSm42MFE";
 
-export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);

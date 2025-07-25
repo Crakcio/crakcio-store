44afea46195ca7d0ff2e40c36a9ce691cba5f44d
Crakcio <150481193+Crakcio@users.noreply.github.com>
2025-07-24 23:35:19 -0500

    Update products.js

--- products.js
+++ products.js
@@ -2,7 +2,7 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // Configura Supabase
 const supabaseUrl = "https://twznikjjvtoedfaxbuvf.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."; // tu clave pública
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3em5pa2pqdnRvZWRmYXhidXZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMyOTI1NDAsImV4cCI6MjA2ODg2ODU0MH0.aOQ10hq-syYqrenvFxveBQj6wKqdDtsDKfykSm42MFE"; // tu clave pública
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // URL base del bucket de imágenes

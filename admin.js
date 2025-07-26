abef405a93d5d3290a5919ae778216e998bc7c57
Crakcio <150481193+Crakcio@users.noreply.github.com>
2025-07-25 21:19:52 -0500

    Update admin.js

--- admin.js
+++ admin.js
@@ -4,7 +4,10 @@ import { supabase } from './supabaseClient.js';
 const form = document.getElementById('agregarProductoForm');
 const adminLista = document.getElementById('adminListaProductos');
 const cerrarSesionAdmin = document.getElementById('cerrarSesionAdmin');
-
+const { data: { session } } = await supabase.auth.getSession();
+if (!session) {
+  window.location.href = 'login.html';
+}
 cerrarSesionAdmin.addEventListener('click', async () => {
   await supabase.auth.signOut();
   window.location.href = 'index.html';

e50b5451e624d66a758fd97d59d274bc236f113a
Crakcio <150481193+Crakcio@users.noreply.github.com>
2025-07-28 08:29:46 -0500

    Update smart-home.js

--- js/smart-home.js
+++ js/smart-home.js
@@ -1,29 +1,9 @@
 // iluminacion.js
-import { cargarProductosPorCategoria } from './products.js';
+import { cargarProductosPorCategoria, mostrarProductos } from './products.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
   const productos = await cargarProductosPorCategoria("smart-home");
-  mostrarProductos(productos);
+  mostrarProductos(productos, "contenedor-productos");
 });
 
-function mostrarProductos(productos) {
-  const container = document.getElementById('contenedor-productos');
-
-  if (productos.length === 0) {
-    container.innerHTML = "<p>No hay productos disponibles aún.</p>";
-    return;
-  }
-
-  productos.forEach(producto => {
-    const div = document.createElement('div');
-    div.classList.add('producto');
-    div.innerHTML = `
-      <img src="${producto.imagen}" alt="${producto.nombre}">
-      <h2>${producto.nombre}</h2>
-      <p>${producto.descripcion}</p>
-      <p>S/ ${producto.precio}</p>
-    `;
-    container.appendChild(div);
-  });
-}
 
